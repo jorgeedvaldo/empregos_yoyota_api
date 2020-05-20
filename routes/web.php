7d482acd23b51fb6dacdd89d8c8831883478a719
yurikiluanji <edvaldojorge19962gmail.com>
2020-05-20 03:14:49 +0100

    adding the ContactController

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index');
 Route::get('/about', 'AboutController@index');
+Route::get('/contact', 'ContactController@index');
