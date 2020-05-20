48abfd15b613b814d102b7082d78791682252173
jorgeedvaldo <edvaldojorge1996@gmail.com>
2020-05-20 05:11:33 +0100

    Save message

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -5,10 +5,20 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Message;
+
 class MessageController extends Controller
 {
     public function create(Request $request)
     {
+        $message = new Message();
 
+        $message->author = $request->author;
+        $message->email = $request->email;
+        $message->subject = $request->subject;
+        $message->content = $request->content;
+
+        $message->save();
+        echo 'Your message has been sent successfully.';
     }
 }
