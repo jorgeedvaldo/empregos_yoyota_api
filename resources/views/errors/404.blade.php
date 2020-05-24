bc082fd2bd9f3ca5731081e33aa49f83487891da
jorgeedvaldo <edvaldojorge1996@gmail.com>
2020-05-24 22:15:54 +0100

    404 error

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('template.app')
+
+@section('content')
+    <div class="d-flex justify-content-center align-items-center" style=" height: 100vh">
+        <h1 class="mr-3 pr-3 align-top border-right inline-block align-content-center">404</h1>
+        <div class="inline-block align-middle">
+            <h2 class="font-weight-normal lead" id="desc">A página que você solicitou não existe.</h2>
+        </div>
+    </div>
+@endsection('content')
