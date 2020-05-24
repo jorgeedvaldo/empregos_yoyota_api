866532a33e2467e0fb9d4efe614de8cb5adba696
jorgeedvaldo <edvaldojorge1996@gmail.com>
2020-05-24 21:58:53 +0100

    Handling errors

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Job;
+use Exception;
 use Illuminate\Http\Request;
 
 class JobController extends Controller
@@ -17,16 +18,31 @@ class JobController extends Controller
 
     public function getById($id)
     {
-        $job = Job::with('categories')->where('id', $id)->get()[0];
-        $categories = Category::get();
-        return view('job', compact('job', 'categories'));
+        try
+        {
+            $job = Job::with('categories')->where('id', $id)->get()[0];
+            $categories = Category::get();
+            return view('job', compact('job', 'categories'));
+        }
+        catch(Exception $ex)
+        {
+            abort(404);
+        }
     }
 
     public function getByCategoryId($id)
     {
-        $categories = Category::get();
-        $category = Category::with('jobs')->where('id', $id)->first();
-        $categoryJobs = $category->jobs;
-        return view('category', compact('category', 'categoryJobs', 'categories'));
+        try
+        {
+            $categories = Category::get();
+            $category = Category::with('jobs')->where('id', $id)->first();
+            $categoryJobs = $category->jobs;
+            return view('category', compact('category', 'categoryJobs', 'categories'));
+        }
+        catch(Exception $ex)
+        {
+            abort(404);
+        }
+
     }
 }
